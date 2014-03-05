c9e31bdd25441effd4efea86c42873475bbc92c1
mbeg <mbeg@nb-nb47.bdt.local>
2014-03-05 09:48:56 +0100

    add sth in comment field

--- GWT_MBEG/src/com/gwt/mbeg/client/GreetingService.java
+++ GWT_MBEG/src/com/gwt/mbeg/client/GreetingService.java
@@ -5,6 +5,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * The client-side stub for the RPC service.
+ * Test
  */
 @RemoteServiceRelativePath("greet")
 public interface GreetingService extends RemoteService {
